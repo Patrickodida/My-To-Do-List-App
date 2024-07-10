14cac283314ad39ca1553eb94da6957919c831fb
Patrickodida <patrick.o.odida@gmail.com>
2024-07-10 15:13:27 +0300

    Format all project files for better display

---
+++ README.md
@@ -0,0 +1,22 @@
+# Project Name
+
+- TODO-List-App
+
+## Description
+
+- Developed using HTML, CSS, and JavaScript, the to-do list application is fully responsive and allows users to input, edit, mark tasks as done, and delete tasks.
+
+### Features
+
+- Add Tasks - Allows the users to add new tasks.
+- Edit tasks - Allows the users to modify tasks.
+- Delete tasks - Provides users with an option to remove tasks.
+- Mark tasks as completed - Allows users to mark tasks as done.
+
+### Clone the App to your local machine
+
+- git clone https://github.com/Patrickodida/My-To-Do-List-App.git
+
+### Contributing
+
+- Feel free to contribute to this project by opening issues and submitting pull requests.
--- main.js
+++ main.js
@@ -4,107 +4,107 @@ const addButton = document.querySelector("button");
 const taskContainer = document.querySelector("ul");
 
 // create the addTask function
-function addTask(task){
-    // create list item
-    let listItem = document.createElement("li");
-    
-    // create a span element for the tasks
-    let taskSpan = document.createElement("span");
-    taskSpan.innerText = task;
+function addTask(task) {
+  // create list item
+  let listItem = document.createElement("li");
 
-    // create a span Element for the icons
-    let spanElement = document.createElement("span");
+  // create a span element for the tasks
+  let taskSpan = document.createElement("span");
+  taskSpan.innerText = task;
 
-    // create Icons
-    // check Icon
-    let checkIcon = document.createElement("input");
-    checkIcon.setAttribute("type", "checkbox");
+  // create a span Element for the icons
+  let spanElement = document.createElement("span");
 
-    // delete Icon
-    let deleteIcon = document.createElement("i");
-    deleteIcon.classList.add("bx", "bx-trash");
+  // create Icons
+  // check Icon
+  let checkIcon = document.createElement("input");
+  checkIcon.setAttribute("type", "checkbox");
 
-    // edit Icon
-    let editIcon = document.createElement("i");
-    editIcon.classList.add("bx", "bx-edit-alt");
-    
-    // done Icon
-    let doneIcon = document.createElement("i");
-    doneIcon.classList.add("bx", "bx-check-circle");
-    doneIcon.style.display = "none";
-    // Add the icons to the spanElement
-    spanElement.append(checkIcon, editIcon, doneIcon, deleteIcon);
-    // Add the spanElement to the listItem
-    listItem.append(taskSpan, spanElement);
-    // Add the listItem to the task container
-    taskContainer.appendChild(listItem);
+  // delete Icon
+  let deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("bx", "bx-trash");
 
-    // Add event Listener to the Icons
-    checkIcon.addEventListener("click", (event) => {
-        if(event.target.checked === true){
-            listItem.style.textDecoration = "line-through";
-            listItem.style.color = "#F04476";
-            input.style.background = "#B58E8D";
-        } else {
-            listItem.style.textDecoration = "none";
-        }
-        saveTaskstoLocalStorage();
-    })
+  // edit Icon
+  let editIcon = document.createElement("i");
+  editIcon.classList.add("bx", "bx-edit-alt");
 
-    editIcon.addEventListener("click", () => {
-        doneIcon.style.display = "inline";
-        taskSpan.contentEditable = true;
-        taskSpan.style.padding = "5px 10px";
-        editIcon.style.display = "none";
-        taskSpan.focus();
-        saveTaskstoLocalStorage();
-    });
+  // done Icon
+  let doneIcon = document.createElement("i");
+  doneIcon.classList.add("bx", "bx-check-circle");
+  doneIcon.style.display = "none";
+  // Add the icons to the spanElement
+  spanElement.append(checkIcon, editIcon, doneIcon, deleteIcon);
+  // Add the spanElement to the listItem
+  listItem.append(taskSpan, spanElement);
+  // Add the listItem to the task container
+  taskContainer.appendChild(listItem);
 
-    doneIcon.addEventListener("click", () => {
-        doneIcon.style.display = "none";
-        taskSpan.contentEditable = false;
-        editIcon.style.display = "inline";
-        saveTaskstoLocalStorage();
-    });
-
-    deleteIcon.addEventListener("click", () => {
-        listItem.remove();
-        saveTaskstoLocalStorage();
-    });
-
-    // save to local storage
+  // Add event Listener to the Icons
+  checkIcon.addEventListener("click", (event) => {
+    if (event.target.checked === true) {
+      listItem.style.textDecoration = "line-through";
+      listItem.style.color = "#F04476";
+      input.style.background = "#B58E8D";
+    } else {
+      listItem.style.textDecoration = "none";
+    }
     saveTaskstoLocalStorage();
+  });
+
+  editIcon.addEventListener("click", () => {
+    doneIcon.style.display = "inline";
+    taskSpan.contentEditable = true;
+    taskSpan.style.padding = "5px 10px";
+    editIcon.style.display = "none";
+    taskSpan.focus();
+    saveTaskstoLocalStorage();
+  });
+
+  doneIcon.addEventListener("click", () => {
+    doneIcon.style.display = "none";
+    taskSpan.contentEditable = false;
+    editIcon.style.display = "inline";
+    saveTaskstoLocalStorage();
+  });
+
+  deleteIcon.addEventListener("click", () => {
+    listItem.remove();
+    saveTaskstoLocalStorage();
+  });
+
+  // save to local storage
+  saveTaskstoLocalStorage();
 }
 
 // Function to save tasks to local storage
-function saveTaskstoLocalStorage(){
-    let tasks = [];
-    let taskItems = document.querySelectorAll("li");
-    taskItems.forEach((item) => {
+function saveTaskstoLocalStorage() {
+  let tasks = [];
+  let taskItems = document.querySelectorAll("li");
+  taskItems.forEach((item) => {
     tasks.push(item.querySelector("span:first-child").innerText);
-});
-localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Function to load tasks from local storage
-function loadTasksFromLocalStorage(){
-    let tasks = JSON.parse(localStorage.getItem("tasks") || []);
-    tasks.forEach((task) => {
-        addTask(task);
-    });
+function loadTasksFromLocalStorage() {
+  let tasks = JSON.parse(localStorage.getItem("tasks") || []);
+  tasks.forEach((task) => {
+    addTask(task);
+  });
 }
 
 // Event listener for adding the task
 addButton.addEventListener("click", () => {
-    let task = input.value.trim();
-    if(task !== ""){
-        addTask(task);
-        // clear the input field
-        input.value = "";
-    }
+  let task = input.value.trim();
+  if (task !== "") {
+    addTask(task);
+    // clear the input field
+    input.value = "";
+  }
 });
 
-// Load items from local storage 
+// Load items from local storage
 window.addEventListener("load", () => {
-    loadTasksFromLocalStorage();
+  loadTasksFromLocalStorage();
 });
